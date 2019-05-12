13801239e8b01e6becfe0f3297dd8105c71e90c5
syo <M.youta.kwg@gmail.com>
2019-05-12 23:31:46 +0900

    filterをfoldLeftとreverseで実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
 object Main {
-  def filter[T](list: List[T])(f: T => Boolean): List[T] = ???
+  def filter[T](list: List[T])(f: T => Boolean): List[T] = {
+    list.foldLeft(Nil: List[T])((acc, e) => if (f(e)) e :: acc else acc).reverse
+  }
 }
